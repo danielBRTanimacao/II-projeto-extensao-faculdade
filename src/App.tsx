042469b05300b30fc05690e2aa1d412084ea90a4
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-02-06 10:27:24 -0300

    feat: creating a desing and base of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 export default () => {
     return (
         <>
-            <main>
-                <h1>Criando um titulo simples para dar inicio a pagina!</h1>
-            </main>
+            <header className="header container">
+                <a href="#Header">Menu secretaria</a>
+                <nav>
+                    <a href="#">cursos</a>
+                    <a href="#">horarios</a>
+                    <a href="#">sobre</a>
+                    <a href="#">liturgia diaria</a>
+                </nav>
+            </header>
+            <div className="second-header" id="Header">
+                <h3>Cursos igreja</h3>
+                <h1>Secretaria igreja</h1>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Minus rem optio amet, aut suscipit laboriosam, totam nulla
+                    deserunt odit distinctio unde minima consequatur ullam
+                    nesciunt deleniti voluptatum magni, qui sapiente?
+                </p>
+            </div>
+            <main>about</main>
         </>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+import "./assets/css/Remedy.css";
+import "./assets/css/Index.css";
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <App />
