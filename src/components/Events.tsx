5665db9809138b8c8be5a16d7b050a09c10ed66e
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-02-08 12:23:25 -0300

    remaking mobile first design

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,8 +1,12 @@
+import Erro404 from "../assets/svg/undraw_page-not-found_6wni.svg";
+
 export default () => {
     return (
         <section className="container" id="horarios">
             <div>
                 <h2>Eventos e horarios</h2>
+                <h4>Ainda em desenvolvimento!</h4>
+                <img src={Erro404} alt="imagem-erro" />
             </div>
         </section>
     );
