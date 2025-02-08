39e7896035645d47908030f545e60f746790695c
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-02-08 12:18:32 -0300

    add new images and base conclusion of carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
+import Sycron from "../assets/svg/undraw_synchronize_txyw.svg";
+import Error404 from "../assets/svg/undraw_page-not-found_6wni.svg";
+
 export default () => {
     const responsive = {
         superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 5 },
@@ -36,26 +39,40 @@ export default () => {
     return (
         <section className="container" id="cursos">
             <div className="course-header">
-                <h2>Cursos recomendados</h2>
-                <p className="lead">
-                    Confira os cursos mais recomendados para você!
-                </p>
+                <h2>
+                    Cursos presenciais e online,{" "}
+                    <span
+                        style={{
+                            fontWeight: "300",
+                            fontSize: ".7em",
+                            letterSpacing: "1px"
+                        }}
+                    >
+                        confira diversos cursos recomendados para você!
+                    </span>
+                </h2>
             </div>
 
             {loading ? (
-                <p>Carregando cursos...</p>
+                <div className="loading-api">
+                    <h4>Carregando cursos...</h4>
+                    <img src={Sycron} alt="sicronizando-img" />
+                </div>
             ) : error ? (
-                <p>Erro ao carregar cursos: {error}</p>
+                <div className="loading-api">
+                    <h4>Erro ao carregar cursos: {error}</h4>
+                    <img src={Error404} alt="sicronizando-img" />
+                </div>
             ) : (
                 <Carousel responsive={responsive}>
                     {data.map((course, index) => (
                         <div
                             key={index}
                             className="view-course"
-                            style={{ margin: "0 5px" }}
+                            style={{ margin: "0 5px", paddingTop: "1.5em" }}
                         >
                             <img src={course.image} alt={course.title} />
-                            <h5>{course.title}</h5>
+                            <h5 style={{ margin: "6px 0" }}>{course.title}</h5>
                             <p>{course.small_description}</p>
                             <aside className="btn-access">
                                 <a href={course.url} target="blanck">
